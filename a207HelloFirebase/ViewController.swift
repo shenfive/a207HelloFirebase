ecbe92a1a1b87b367f339a9c8f4c6e7ef2a283a2
Danny Shen <shenfive@gmail.com>
2020-08-29 10:58:16 +0800

    Hello Firebase

--- a207HelloFirebase/ViewController.swift
+++ a207HelloFirebase/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Auth.auth().signInAnonymously { (result, error) in
+            if error != nil{
+                print(error?.localizedDescription)
+            }else{
+                print(result?.user.debugDescription)
+            }
+        }
+        
+        
     }
 
 
